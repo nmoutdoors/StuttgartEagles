3a4fa3fef1c068e13c427914b5834e31880b28f6
Scott Underwood <nmoutdoors@gmail.com>
2016-02-06 20:19:27 +0100

    Added next-three-tournaments section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
 
+$("i").addClass("animated bounce");
+$("#box1").addClass("animated fadeInLeft");
+$("#box1-title").text("Easter Euro Cup");
+$("#box1-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box2").addClass("animated fadeInDown");
+$("#box2-title").text("United World Games");
+$("#box2-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box3").addClass("animated fadeInRight");
+$("#box3-title").text("Speyer Tournament");
+$("#box3-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+});
