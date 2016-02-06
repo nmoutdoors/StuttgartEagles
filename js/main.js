8384da6f76b01939ec172204142dff943578455e
Scott Underwood <nmoutdoors@gmail.com>
2016-02-07 00:36:28 +0100

    Three Tournaments completed

--- js/main.js
+++ js/main.js
@@ -13,4 +13,17 @@ $("#box3").addClass("animated fadeInRight");
 $("#box3-title").text("Speyer Tournament");
 $("#box3-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
 
+$("i").addClass("animated bounce");
+$("#box12").addClass("animated fadeInLeft");
+$("#box12-title").text("Easter Euro Cup");
+$("#box12-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box22").addClass("animated fadeInDown");
+$("#box22-title").text("United World Games");
+$("#box22-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box32").addClass("animated fadeInRight");
+$("#box32-title").text("Speyer Tournament");
+$("#box32-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
 });
